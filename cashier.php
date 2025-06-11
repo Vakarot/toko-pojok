409f441eab18970e7839894e0ac2b17f777b406a
dahliapril01 <dahliaapriliyana@gmail.com>
2025-06-12 06:59:19 +0700

    cashier style lagi

--- cashier.php
+++ cashier.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="inventoryStyle.css">
+    <link rel="stylesheet" href="cashierStyle.css">
     <title>Cashier</title>
 </head>
 <body>
